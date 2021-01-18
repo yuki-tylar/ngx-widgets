21bc7fd6f0446f11a3d6a6682ff339a175b25c86
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-01-17 21:26:44 -0800

    moved reusable function to service

--- projects/form/src/lib/checkbox/checkbox.component.ts
+++ projects/form/src/lib/checkbox/checkbox.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
-
+import { FormService, ColorPreset } from '../form.service';
 
 @Component({
   selector: 'ng2-checkbox',
@@ -14,24 +14,21 @@ export class CheckboxComponent implements OnInit {
   @Input() checked: boolean = false;
   @Input() color: ColorPreset | string = '';
 
-  @Output() changeState = new EventEmitter<ChangeStateEvent>();
+  @Output() change = new EventEmitter<ChangeStateEvent>();
 
   public _id: string;
-  public _label: string;
-  public _checked: boolean;
-  public _color: string;
+  public _label: string = 'Please check me!';
+  public _checked: boolean = false;
+  public _color: string = '';
 
   private host: HTMLElement;
   private input: HTMLInputElement | null = null;
 
   constructor(
     _el: ElementRef,
+    private _f: FormService,
   ) {
-    this._id = createRandom();
-    this._label = 'Please check me!';
-    this._checked = false;
-    this._color = '';
-
+    this._id = _f.createId();
     this.host = _el.nativeElement;
   }
 
@@ -39,17 +36,7 @@ export class CheckboxComponent implements OnInit {
     if(this.id){ this._id = this.id.toString(); }
     if(this.label){ this._label = this.label; }
     if(this.checked === true){ this._checked = this.checked; }
-
-    var c = this._color;
-    switch(this.color){
-      case 'blue' :  c = '#3D5AFE'; break;
-      case 'green':  c = '#43A047'; break;
-      case 'orange': c = '#EF6C00'; break;
-      case 'black':  c = '#212121'; break;
-      case '':       c = '#212121'; break;
-      default:       c = this.color; break;
-    }
-    this._color = c;
+    this._color = this._f.setColor(this.color);
 
     this.input = this.host.querySelector('input');
     if(this._checked){ this.check(false); }
@@ -59,12 +46,12 @@ export class CheckboxComponent implements OnInit {
   check(emit: boolean = true){
     this._checked = true;
     if(this.input){ this.input.checked = true; }
-    if(emit){ this.changeState.emit({id: this._id, checked: true}); }
+    if(emit){ this.change.emit({id: this._id, checked: true}); }
   }
   uncheck(emit: boolean = true){
     this._checked = false;
     if(this.input){ this.input.checked = false; }
-    if(emit){ this.changeState.emit({id: this._id, checked: false}); }
+    if(emit){ this.change.emit({id: this._id, checked: false}); }
   }
 
   onChange(e: Event){
@@ -75,14 +62,6 @@ export class CheckboxComponent implements OnInit {
   }
 }
 
-function createRandom (){
-  var strSet = 'abcdefghijklmnopqrstuvwxyz1234567890'; 
-  var result = '';
-  for(var i=0; i<8; i++){ result += strSet[Math.floor( Math.random() * strSet.length )]; }
-  return result;
-}
-
-type ColorPreset = 'blue' | 'green' | 'orange' | 'black'
 type ChangeStateEvent = {
   checked: boolean;
   id: string;
--- projects/form/src/lib/form.service.ts
+++ projects/form/src/lib/form.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class FormService {
 
   constructor() { }
+
+  createId(): string{
+    var strSet = 'abcdefghijklmnopqrstuvwxyz1234567890'; 
+    var result = '';
+    for(var i=0; i<8; i++){ result += strSet[Math.floor( Math.random() * strSet.length )]; }
+    return result;
+  }
+
+  setColor(colorname: string): string{
+    var c = '';
+    switch(colorname){
+      case 'blue' :  c = '#3D5AFE'; break;
+      case 'green':  c = '#43A047'; break;
+      case 'orange': c = '#EF6C00'; break;
+      case 'black':  c = '#212121'; break;
+      case '':       c = '#212121'; break;
+      default:       c = colorname; break;
+    }
+    return c;
+  }
 }
+
+export type ColorPreset = 'blue' | 'green' | 'orange' | 'black'
