dd6eb9370e489b2be0e4c8ac8729974add1b87ce
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-01-17 21:24:46 -0800

    updated

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import{ FormModule } from 'form';
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormModule
   ],
   providers: [],
