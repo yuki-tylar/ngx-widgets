1eafabf565ec41a39359a43dd890301f0f3f229f
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-01-25 21:37:48 -0800

    some changes

--- projects/form/src/lib/form.service.ts
+++ projects/form/src/lib/form.service.ts
@@ -6,11 +6,4 @@ import { Injectable } from '@angular/core';
 export class FormService {
 
   constructor() { }
-
-  createId(): string{
-    var strSet = 'abcdefghijklmnopqrstuvwxyz1234567890'; 
-    var result = '';
-    for(var i=0; i<8; i++){ result += strSet[Math.floor( Math.random() * strSet.length )]; }
-    return result;
-  }
 }
