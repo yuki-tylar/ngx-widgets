76f5ed5a6b58e77fc1a693b3f66481bc671bc94f
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-01-29 21:50:49 -0800

    setValidator func made

--- projects/form/src/lib/form-item-controller.ts
+++ projects/form/src/lib/form-item-controller.ts
@@ -17,7 +17,7 @@ export class FormItemController implements IFormItemController{
   private _id: string = '';
   private _isTouched: boolean = false;
   private _isDirty: boolean = false;
-  private _validators: Map<FormValidationName, {isValid: boolean, message: string}> = new Map();
+  private _validators: Map<FormValidationName, {isValid: boolean, message: string, value?: number}> = new Map();
 
   get id(){ return this._id; }
   get isTouched(){ return this._isTouched; }
@@ -36,13 +36,16 @@ export class FormItemController implements IFormItemController{
 
   get errors(){
     const errors: FormValidationName[] = [];
-    this._validators.forEach((v,k)=>{ if(v.isValid){ errors.push(k); } })
+    this._validators.forEach((v,k)=>{ if(!v.isValid){ errors.push(k); } })
     return errors;
   }
   get errorMessage(){
     let message: string = '';
-    const required = this._validators.get('required');
-    if(required && !required.isValid){ message = required.message; }
+    const errors = this.errors;
+    if(errors.length > 0){
+      const error = this._validators.get(errors[0]);
+      if(error){ message = error.message; }
+    }
     return message;
   }
 
@@ -60,4 +63,34 @@ export class FormItemController implements IFormItemController{
   touch(){ this._isTouched = true; }
   dirty(){ this._isDirty = true; }
   clean(){ this._isTouched = false; this._isDirty = false; }
+
+  setValidatorRequired(o: {required?: string | boolean, message?: string}): boolean{
+    if(o.required === undefined || o.required === false || o.required === 'false'){ return false; }
+    
+    if(!o.message || o.message.length == 0){ o.message = 'Required!'; }
+    this.validators.set('required', {isValid: false, message: o.message});
+    return true;
+  }
+
+  setValidatorMin(o: {min?: string | number, message?: string}): boolean{
+    const val = Number(o.min);
+    if(isNaN(val) || val <= 0){ return false; }
+
+    if(!o.message || o.message.length == 0){ o.message = 'Please select more than {}'; }
+    const message = o.message.replace('{}', val.toString());
+
+    this.validators.set('min', {isValid: false, message: message, value: val});
+    return true;
+  }
+
+  setValidatorMax(o: {max?: string | number, message?: string}): boolean{
+    const val = Number(o.max);
+    if(isNaN(val) || val <= 0){ return false; }
+
+    if(!o.message || o.message.length == 0){ o.message = 'Please select less than {}'; }
+    const message = o.message.replace('{}', val.toString());
+
+    this.validators.set('max', {isValid: false, message: message, value: val});
+    return true;
+  }
 }
