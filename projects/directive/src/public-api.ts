a0e9fd8daf271b473dee3e5f20f93819af85c05e
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-02-04 20:49:41 -0800

    detatch directive library

--- projects/directive/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of directive
- */
-
-export * from './lib/directive.service';
-export * from './lib/focus.directive';
-export * from './lib/click-outside.directive';
-export * from './lib/directive.module';
